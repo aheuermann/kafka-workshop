22519ac5615f617b1846a080c566b5b67dedea85
Jan Monterrubio <jan.monterrubio@c2fo.com>
2023-05-19 17:03:43 -0500

    run container in network

--- scripts/db_query_order_status.sh
+++ scripts/db_query_order_status.sh
@@ -3,9 +3,9 @@
 # Set variables
 DBUSER='postgres'
 PGPASSWORD='password'
-DBHOST='host.docker.internal'
+DBHOST='postgres'
 DBNAME='app'
 QUERY='SELECT * FROM public.order_status;'
 
 # Run the query using psql
-docker run -i --rm -e PGPASSWORD=$PGPASSWORD postgres:15 psql -w -h $DBHOST -p 5432 -U $DBUSER -d $DBNAME -c "$QUERY"
+docker run -i --rm --network=kafka-workshop_default -e PGPASSWORD=$PGPASSWORD postgres:15 psql -w -h $DBHOST -p 5432 -U $DBUSER -d $DBNAME -c "$QUERY"
